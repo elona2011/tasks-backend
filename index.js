c8f073daf0c9f0484e5b329dc2b937fb91bb860b
elona2011 <arthuxx@gmail.com>
2020-06-01 21:29:44 +0800

    update

--- index.js
+++ index.js
@@ -7,19 +7,46 @@ const { createHash } = require('crypto');
 const app = new Koa();
 const router = new Router();
 
+app.context.getXmlValue = function(field){
+    if (!this.xmlData) return
+    return this.xmlData[field][0]
+}
+app.context.getOpenId = function() {
+    if (this.openId) return this.openId
+    let openId = this.openId = this.getXmlValue("FromUserName")
+    return openId
+}
+app.context.xmlBuilder = new xml2js.Builder({headless: true, cdata:true, rootName: "xml"});
+
 router.get('/wx', (ctx, next) => {
-    let { echostr, timestamp, nonce, signature } = ctx.request.query
-    let arr = [token, timestamp, nonce]
-    arr.sort()
-    let str = arr.join("")
-    let shaStr = sha1(str)
-    if (shaStr === signature) {
-        ctx.body = echostr
-        debugger
-    } else {
-        console.log(arr)
-        ctx.body = "wrong"
+    // ctx.router available
+    let body = ctx.request.body
+    let xmlData = body.xml
+    ctx.xmlData = xmlData
+    let type = ctx.getXmlValue("MsgType")
+    ctx.msgType = type
+    await next()
+    console.log('res body', ctx.body)
+    let userOpenId = xmlData.FromUserName[0]
+    let myId = xmlData.ToUserName[0]
+    let replyObject = {
+        ToUserName: userOpenId,
+        FromUserName: myId,
+        CreateTime: xmlData.CreateTime[0]
     }
+    let ret = Object.assign(replyObject, ctx.body)
+    // var responseObj = {
+    //     "ToUserName": `${xmlData.FromUserName[0]}`,
+    //     "FromUserName": `${xmlData.ToUserName[0]}`,
+    //     "CreateTime":xmlData.CreateTime[0],
+    //     "MsgType": `text`,
+    //     "Content": `hi2 ${xmlData.Content[0]}`
+
+    // };
+    ctx.set('Content-Type', 'text/xml');
+    // var builder = new xml2js.Builder({headless: true, cdata:true, rootName: "xml"});
+    var xml = ctx.xmlBuilder.buildObject(ret)
+    ctx.body = xml
 });
 
 app.use(xmlParser())
@@ -37,7 +64,10 @@ app.use(router.routes())
 
 // response
 app.use(ctx => {
-    ctx.body = 'Hello Koa';
+    ctx.body = {
+        Content: 'aaab',
+        MsgType: 'text'
+    };
 });
 
 app.listen(port, function () {
@@ -51,18 +81,18 @@ const encrypt = (algorithm, content) => {
     return hash.digest('hex')
 }
 const sha1 = (content) => encrypt('sha1', content)
-// app.get('/wx', function (req, res) {
-//     let { echostr, timestamp, nonce, signature } = req.query
-//     let token = "wtftoken"
+
+// router.get('/wx', (ctx, next) => {
+//     let { echostr, timestamp, nonce, signature } = ctx.request.query
 //     let arr = [token, timestamp, nonce]
 //     arr.sort()
 //     let str = arr.join("")
 //     let shaStr = sha1(str)
 //     if (shaStr === signature) {
-//         res.send(echostr)
+//         ctx.body = echostr
 //         debugger
 //     } else {
 //         console.log(arr)
-//         res.send("wrong")
+//         ctx.body = "wrong"
 //     }
 // });
