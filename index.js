f12db0e385260c17a43ddf9fb3eccd6dbc8e4e69
elona2011 <arthuxx@gmail.com>
2020-06-01 20:08:44 +0800

    test wx

--- index.js
+++ index.js
@@ -1,13 +1,25 @@
 const Koa = require('koa');
 const xmlParser = require('koa-xml-body')
 const Router = require("@koa/router")
-const {createHash}= require('crypto');
+const { createHash } = require('crypto');
 
 const app = new Koa();
 const router = new Router();
 
-router.get('/', (ctx, next) => {
-    // ctx.router available
+router.get('/wx', (ctx, next) => {
+    let { echostr, timestamp, nonce, signature } = ctx.request.query
+    let token = "wtftoken"
+    let arr = [token, timestamp, nonce]
+    arr.sort()
+    let str = arr.join("")
+    let shaStr = sha1(str)
+    if (shaStr === signature) {
+        ctx.body = echostr
+        debugger
+    } else {
+        console.log(arr)
+        ctx.body = "wrong"
+    }
 });
 
 app.use(router.routes())
@@ -24,23 +36,23 @@ app.listen(3000, function () {
 
 // for wx config
 const encrypt = (algorithm, content) => {
-  let hash = createHash(algorithm)
-  hash.update(content)
-  return hash.digest('hex')
+    let hash = createHash(algorithm)
+    hash.update(content)
+    return hash.digest('hex')
 }
 const sha1 = (content) => encrypt('sha1', content)
-app.get('/wx', function(req, res) {
-    let {echostr, timestamp, nonce, signature} = req.query
-    let token="wtftoken"
-    let arr = [token, timestamp, nonce]
-    arr.sort()
-    let str = arr.join("")
-    let shaStr = sha1(str)
-    if (shaStr === signature) {
-        res.send(echostr)
-        debugger
-    } else {
-        console.log(arr)
-        res.send("wrong")
-    }
-});
+// app.get('/wx', function (req, res) {
+//     let { echostr, timestamp, nonce, signature } = req.query
+//     let token = "wtftoken"
+//     let arr = [token, timestamp, nonce]
+//     arr.sort()
+//     let str = arr.join("")
+//     let shaStr = sha1(str)
+//     if (shaStr === signature) {
+//         res.send(echostr)
+//         debugger
+//     } else {
+//         console.log(arr)
+//         res.send("wrong")
+//     }
+// });
