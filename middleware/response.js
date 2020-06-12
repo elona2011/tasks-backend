ca71f7da024246f6167f99c4c6d00dc09d97ca73
elona2011 <arthuxx@gmail.com>
2020-06-12 19:57:05 +0800

    update

--- middleware/response.js
+++ middleware/response.js
@@ -1,12 +1,12 @@
 const response = async ctx => {
     if (ctx.Content == '发布') {
         ctx.body = {
-            Content: `<a href="http://34.80.134.190/#/publish/${ctx.jwtToken}">发布任务</a>`,
+            Content: `<a href="http://p.wechat.ac.cn/#/publish/${ctx.jwtToken}">发布任务</a>`,
             MsgType: 'text'
         };
     } else {
         ctx.body = {
-            Content: `<a href="http://34.80.134.190/#/usertasksnew/${ctx.jwtToken}">点击做任务</a>`,
+            Content: `<a href="http://p.wechat.ac.cn/#/usertasksnew/${ctx.jwtToken}">点击做任务</a>`,
             MsgType: 'text'
         };
     }
