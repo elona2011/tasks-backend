b360500c425c8283d0bdd3c6f72fd36d022163ff
elona2011 <arthuxx@gmail.com>
2020-07-21 11:05:45 +0800

    update

--- routes/dy.js
+++ routes/dy.js
@@ -3,17 +3,14 @@ const { accessToken } = require('../interfaces/dy')
 const jwt = require('jsonwebtoken')
 const { jwt_key } = require('../config')
 const { addUser } = require('../sql/account')
-const { getOk, getRes } = require('../returnCode')
+const { getOk } = require('../returnCode')
 
 const router = new Router({ prefix: '/dy' });
 
 router.get('/access_token', async (ctx) => {
-    console.log('/access_token', ctx.request.body)
-    console.log('ctx.request.query.code', ctx.request.query.code)
     let r = await accessToken(ctx.request.query.code)
-    console.log('r', r)
-    ctx.jwtToken = jwt.sign({ openid: r.openid }, jwt_key)
-    await addUser(r.openid, ctx.jwtToken, r.access_token)
+    ctx.jwtToken = jwt.sign({ openid: r.open_id }, jwt_key)
+    await addUser(r.open_id, ctx.jwtToken, r.access_token)
     ctx.body = getOk(ctx.jwtToken)
 })
 
